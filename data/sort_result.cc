705b53e9d3459ecacf5abe8f66ed7305fb76ece7
Jamis <jamis@localhost.localdomain>
2015-07-12 22:48:16 +0800

    fix bug

--- data/sort_result.cc
+++ data/sort_result.cc
@@ -31,21 +31,27 @@ int main(int, char**) {
     fin.close();
     fin.open("pagerank_result");
 
-    char chr;
-    int state = 0;
 
     vector< pair<float, uint32_t> > sorted_results;
+    sorted_results.reserve(67108864);
+    std::cout << "Memory allocated. " << std::endl;
 
     auto handle_line = [&sorted_results](const std::string& line) {
-        uint32_t url = line[0] << 24 | line[1] << 16 | 
-                       line[2] <<  8 | line[3] <<  0;
+        uint32_t url = (uint32_t(line[0]) & 0xff)<< 24 | 
+                       (uint32_t(line[1]) & 0xff)<< 16 | 
+                       (uint32_t(line[2]) & 0xff)<<  8 | 
+                       (uint32_t(line[3]) & 0xff)<<  0;
 
         float pr = *(float*)(line.data() + 5);
 
         sorted_results.emplace_back(make_pair(pr, url));
     };
 
+    size_t counter = 0;
 
+
+    char chr;
+    int state = 0;
     line.clear();
     while (fin.get(chr)) {
         line += chr;
@@ -55,18 +61,23 @@ int main(int, char**) {
                 break;
             case 1:
                 if (chr == 'B') state = 2;
+                else state = 0;
                 break;
             case 2:
                 if (chr == 'C') state = 3;
+                else state = 0;
                 break;
             case 3:
                 if (chr == 'D') state = 4;
+                else state = 0;
                 break;
             case 4:
                 if (chr == 'E') state = 5;
+                else state = 0;
                 break;
             case 5:
                 if (chr == 'F') state = 6;
+                else state = 0;
                 break;
             case 6:
                 if (chr == '\n') {
@@ -74,7 +85,9 @@ int main(int, char**) {
                     line.resize(line.length() - 7);
                     handle_line(line);
                     line.clear();
-                }
+                    ++counter;
+                    if (counter % 131072 == 0) std::cout << counter << std::endl;
+                } else state = 0;
                 break;
         }
     }
@@ -83,7 +96,12 @@ int main(int, char**) {
 
     std::ofstream fout("sorted_pagerank_result");
 
-    for (const auto i: sorted_results) 
+    for (const auto i: sorted_results) {
+        if (i.second >= dict.size()) {
+            std::cerr << "Error: i == " << i.second << ", sizeof(dict) == " << dict.size() << std::endl;
+            return 1;
+        }
         fout << dict[i.second] << ' ' << i.first << '\n';
+    }
     
 }
