c8ffa7dd7af242be7cea623fc7722a2648be02db
Jamis <hoojamis@gmail.com>
2015-07-13 20:17:35 +0800

    fix bug

--- src/pagerank_result_sort.cc
+++ src/pagerank_result_sort.cc
@@ -13,30 +13,9 @@
  *  Description: sort map-reduce result
  *****************************************************************************/
 #include <iostream>
-#include <cstring>
-#include <cassert>
 
 #include "hadoop/Pipes.hh"
 #include "hadoop/TemplateFactory.hh"
-
-void host_to_network(uint32_t x, std::string& str) {
-    str[0] = x >> 24;
-    str[1] = x >> 16;
-    str[2] = x >>  8;
-    str[3] = x >>  0;
-}
-
-uint32_t network_to_host(const void* from) {
-    uint8_t* network = (uint8_t*)(from);
-    return uint32_t(network[0]) << 24 | 
-           uint32_t(network[1]) << 16 |
-           uint32_t(network[2]) <<  8 |
-           uint32_t(network[3]) <<  0;
-}
-
-// assert url_list_header.length() > 4 Bytes
-const std::string url_list_header = "     ";
-const std::string delimiter = "ABCDEF";
 
 /* Mapper input format:
    Key = ...
@@ -50,7 +29,16 @@ public:
     
     void map(HadoopPipes::MapContext& context) {
         std::string input_value = context.getInputValue();
-        context.emit(std::string(input_value, 0, 4), std::string(input_value, 5, 4));
+
+        std::string emit_key(4, 0);
+        emit_key[0] = input_value[8];
+        emit_key[1] = input_value[7];
+        emit_key[2] = input_value[6];
+        emit_key[3] = input_value[5];
+
+        std::string emit_value(input_value, 0, 4);
+
+        context.emit(emit_key, emit_value);
     }        
 };
 
@@ -64,15 +52,10 @@ public:
     PageRankResultSorterReducer(HadoopPipes::TaskContext& /* context */) { }
     
     void reduce(HadoopPipes::ReduceContext& context) {
-        std::string input_value = context.getInputValue();
+        std::string emit_key = context.getInputKey();
 
-        std::string emit_key(4, 0);
-        emit_key[0] = input_value[3];
-        emit_key[1] = input_value[2];
-        emit_key[2] = input_value[1];
-        emit_key[3] = input_value[0];
-
-        context.emit(emit_key, context.getInputKey());
+        while (context.nextValue()) 
+            context.emit(emit_key, context.getInputValue());
     }
 };
 
